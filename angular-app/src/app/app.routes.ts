3f4f783cf472abb07b2ce85433f4c1e770fc5ba8
SarahMaudeG <206232222@cegepsherbrooke.qc.ca>
2025-11-22 15:44:23 -0500

    Début page Livres

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AproposComponent } from './apropos/apropos.component';
 import { HomeComponent } from './home/home.component';
+import { LivresComponent } from './pages/livres/livres';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'apropos', component: AproposComponent }
+  { path: 'apropos', component: AproposComponent },
+  { path: 'livres', component: LivresComponent }
 ];
